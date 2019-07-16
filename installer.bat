fe51b5b01b9c0670efbdf20e07a3a1d231c75ee0
ThinkHao <jinxinhaowx@gmail.com>
2019-07-16 12:34:57 +0800

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,4 +1,6 @@
 @echo off
+rem 安装Chocolatey
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 rem 设置Chocolatey的安装目录
 setx ChocolateyInstall D:\Chocolatey /M
 rem 刷新环境变量，使安装目录的配置生效
